e5a93024f6f59224adc9cf31438ac819541a1c64
yonggyu.song <ygddabong@gmail.com>
2020-04-08 23:49:42 +0900

    Hide google api key

--- KoreanTimeBlocker/AppDelegate.swift
+++ KoreanTimeBlocker/AppDelegate.swift
@@ -18,8 +18,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        GMSServices.provideAPIKey("AIzaSyAKeGVVjoZWuB4KAFghHlyWP6N-rKAa6ac")
-        GMSPlacesClient.provideAPIKey("AIzaSyAKeGVVjoZWuB4KAFghHlyWP6N-rKAa6ac")
+        GMSServices.provideAPIKey("API KEY")
+        GMSPlacesClient.provideAPIKey("API KEY")
         return true
     }
 
